55f87c2c5553996652b116d697e40fa14d39ff91
thoryur@gmail.com <your.email@example.com>
2023-08-05 20:46:59 +0300

    map of functions created

--- main/main.go
+++ main/main.go
@@ -1,1 +1,28 @@
-aaaa
+package main
+
+import (
+	"fmt"
+
+	"github.com/Pomog/autofix/functions"
+)
+
+var functionMap = make(map[string]func())
+
+func addToFunctionMap(fnName string, fn func()) {
+	functionMap[fnName] = fn
+}
+
+func main() {
+	// Add registered functions to the functionMap
+	for _, fn := range functions.RegisteredFunctions {
+		addToFunctionMap(fn.Name, fn.Fn)
+	}
+
+	condition := "Funct1"
+
+	if chosenFunction, exists := functionMap[condition]; exists {
+		chosenFunction()
+	} else {
+		fmt.Println("No function found for the given condition")
+	}
+}
