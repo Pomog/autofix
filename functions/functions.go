6712a854098a834875f75d8663c205a3ae8e8586
thoryur@gmail.com <your.email@example.com>
2023-08-06 22:48:38 +0300

    ReplaceHexWithDecimal

--- functions/functions.go
+++ functions/functions.go
@@ -2,6 +2,8 @@ package functions
 
 import (
 	"fmt"
+	"regexp"
+	"strconv"
 )
 
 var RegisteredFunctions = []struct {
@@ -9,8 +11,28 @@ var RegisteredFunctions = []struct {
 	Fn   func()
 }{
 	{"Funct1", Funct1},
+	{"Funct2", WrapReplaceHexWithDecimal},
 }
 
 func Funct1() {
 	fmt.Println("Funct1 is called")
+}
+
+func WrapReplaceHexWithDecimal() {
+	input := "1E (hex) files were added. 2A (hex) items are available."
+	output := ReplaceHexWithDecimal(input)
+	fmt.Println(output)
+}
+
+func ReplaceHexWithDecimal(input string) string {
+	re := regexp.MustCompile(`([0-9A-Fa-f]+) \(hex\)`)
+	output := re.ReplaceAllStringFunc(input, func(match string) string {
+		hexStr := re.FindStringSubmatch(match)[1]
+		decimalValue, err := strconv.ParseInt(hexStr, 16, 64)
+		if err != nil {
+			return match
+		}
+		return fmt.Sprintf("%d", decimalValue)
+	})
+	return output
 }
